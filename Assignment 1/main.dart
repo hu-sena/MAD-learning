04453249d90807f995717ec74f9a47d3d38b2554
Husna Zubir <shusnazubir@gmail.com>
2022-04-13 14:46:11 +0800

    Skeleton Code

--- Assignment 1/main.dart
+++ Assignment 1/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // which class is stateful
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {}
+}
