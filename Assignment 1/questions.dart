c48ae88e5cbf4ca65038e9a2d447997e35e4c6c1
Husna Zubir <shusnazubir@gmail.com>
2022-04-14 20:34:59 +0800

    question customed widget

---
+++ Assignment 1/questions.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Questions extends StatelessWidget {
+  final String questionText;
+  Questions(this.questionText);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.all(22),
+        child: Text(
+          questionText,
+          style: TextStyle(fontSize: 20),
+          textAlign: TextAlign.center,
+        ));
+  }
+}
