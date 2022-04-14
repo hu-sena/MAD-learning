3cd71eb871e6b5881519969b6024b9d68ed716db
Husna Zubir <shusnazubir@gmail.com>
2022-04-14 20:34:10 +0800

    insert if else and textbox

--- Assignment 1/main.dart
+++ Assignment 1/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'questions.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,6 +7,7 @@ void main() {
 
 class MyApp extends StatefulWidget {
   // which class is stateful
+  @override
   State<StatefulWidget> createState() {
     return _MyAppState();
   }
@@ -13,5 +15,37 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    final inputController = TextEditingController();
+    var answer = ['Apple', 'Banana', 'Mango'];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Assignment 1'),
+        ),
+        body: Column(children: <Widget>[
+          Questions('Enter a number:'),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(controller: inputController),
+          ),
+          RaisedButton(
+            child: Text('Show Answer'),
+            onPressed: () {
+              if (inputController.text == '1') {
+                print(answer.elementAt(0));
+              }
+              if (inputController.text == '2') {
+                print(answer.elementAt(1));
+              }
+              if (inputController.text == '3') {
+                print(answer.elementAt(2));
+              }
+            },
+          ),
+        ]),
+      ),
+    );
+  }
 }
